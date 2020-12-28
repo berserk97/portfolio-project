03df9357de338bb5104b536b2b560f275b298ef0
Saif Malek <saifad840@gmail.com>
2020-12-28 17:23:11 +0500

    made blog app skeleton

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+import datetime
 
-# Create your models here.
+class Blog(models.Model):
+    title =         models.CharField(max_length=200)
+    description =   models.TextField()
+    date =          models.DateField(default=datetime.date.today)
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from pf_items import views
@@ -22,6 +22,7 @@ from pf_items import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('blog/', include('blog.urls')),
 
 ]
 
