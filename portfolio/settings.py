5e012f35fe8bfd28e2a34d074bcd2271e7e72ce9
saif <saifad840@gmail.com>
2021-01-13 10:50:45 +0000

    Added .gitignore and modified settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'y^j5b820($))9g-l%fkug8(#!2tn4@o^rvqta62t-d)w92l236'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['saifullah744.pythonanywhere.com']
 
 
 # Application definition
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
-
 MEDIA_ROOT = BASE_DIR / 'media'
